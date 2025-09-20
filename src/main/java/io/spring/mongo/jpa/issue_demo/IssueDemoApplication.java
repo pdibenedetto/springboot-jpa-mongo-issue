6a8a14d4f38cb92743b7ade8b7c700d48cbe6bdc
Paul DiBenedetto <padibenedetto.ctr@kr.af.mil>
2025-09-20 11:31:52 -0400

    Initial commit

---
+++ src/main/java/io/spring/mongo/jpa/issue_demo/IssueDemoApplication.java
@@ -0,0 +1,13 @@
+package io.spring.mongo.jpa.issue_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/mongo/jpa/issue_demo/IssueDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.mongo.jpa.issue_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
