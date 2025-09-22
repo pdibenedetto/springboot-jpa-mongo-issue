c25b58bfad07e94d76d816de4118b3c1a4ed97a1
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2025-09-22 18:54:56 -0400

    Removing inheritance example to keep it simple

--- src/main/java/io/spring/mongo/jpa/issue/jpa/inheritance/ShopperRepository.java
+++
@@ -1,9 +0,0 @@
-package io.spring.mongo.jpa.issue.jpa.inheritance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShopperRepository extends JpaRepository<Shopper, Long> {
-
-}
