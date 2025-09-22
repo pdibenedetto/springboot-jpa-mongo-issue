116fbede6859dc0506f6fd1d544ed2f3e634aff7
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2025-09-22 18:30:10 -0400

    Patching for simple use cases with inheritence as well

---
+++ src/main/java/io/spring/mongo/jpa/issue/jpa/UserRepository.java
@@ -0,0 +1,8 @@
+package io.spring.mongo.jpa.issue.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
