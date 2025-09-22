11c6be713e91cb90cabcd2d131d595ddd9a7a735
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2025-09-22 16:22:23 -0400

    Undoing

--- src/main/java/io/spring/mongo/jpa/issue/mongo/EventRepository.java
+++ src/main/java/io/spring/mongo/jpa/issue/mongo/EventRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends Repository<Event, String> {
+public interface EventRepository extends MongoRepository<Event, String> {
 }
