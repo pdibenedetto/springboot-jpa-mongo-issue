2b6a764f6a9c354f130a88d2ad9b79eb116da812
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2025-09-22 18:51:56 -0400

    Removing comments

--- src/main/java/io/spring/mongo/jpa/issue/jpa/inheritance/ShopperRepository.java
+++ src/main/java/io/spring/mongo/jpa/issue/jpa/inheritance/ShopperRepository.java
@@ -4,10 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-//public interface ShopperRepository extends EntityRepository<Shopper>{
-//
-//}
-
 public interface ShopperRepository extends JpaRepository<Shopper, Long> {
 
 }
